8a56182b40f7f0249da6aae6a68ec20bcb6a63a1
MIMJA156 <75694661+MIMJA156@users.noreply.github.com>
2023-05-11 15:33:48 -0600

    Update sketch_apr25a.ino

--- sketch_apr25a/sketch_apr25a.ino
+++ sketch_apr25a/sketch_apr25a.ino
@@ -3,15 +3,12 @@
 #include <WiFi.h>
 #include "Pin.h"
 
-const char *SSID = "Conner-and-Miles";
+const char *SSID = "Miles-Quint-Ashton";
 const char *PASSWORD = "123456789";
 
 WebServer server(80);
 
-Pin pin26(26, LOW);
-Pin pin27(27, LOW);
-Pin pin25(25, LOW);
-Pin pin33(33, LOW);
+Pin pin15(15, LOW);
 
 
 void setup() {
@@ -21,25 +18,9 @@ void setup() {
     client.println(getHtml());
   });
 
-  server.get("/api/26-toggle", [](WiFiClient client) {
-    pin26.toggle();
-    Serial.println(2);
-    client.println(pin26.getState() + "");
-  });
-
-  server.get("/api/27-toggle", [](WiFiClient client) {
-    pin27.toggle();
-    client.println(pin27.getState() + "");
-  });
-
-  server.get("/api/25-toggle", [](WiFiClient client) {
-    pin25.toggle();
-    client.println(pin25.getState() + "");
-  });
-
-  server.get("/api/33-toggle", [](WiFiClient client) {
-    pin33.toggle();
-    client.println(pin33.getState() + "");
+  server.get("/api/toggle", [](WiFiClient client) {
+    pin15.toggle();
+    Serial.println("toggle");
   });
 
   server.begin(SSID, PASSWORD);
@@ -50,5 +31,5 @@ void loop() {
 }
 
 String getHtml(){
-return "<!DOCTYPE html><html><head><title>Arduino Control</title></head><body><style>html,body {height: 100%;overflow-y: auto;font-family: Arial;}button.main-button {border-radius: 10px;font-size: 30px;background-color: #03CC29;color: white;padding: 10px;margin: 10px;border-color: #02ad22;transition: 0.3s;}div.monitor {border-radius: 10px;font-size: 30px;background-color: #c4c4c4;color: white;padding: 10px;margin: 5px;transition: 0.3s;}button.main-button:hover {background-color: #0CD833;}h1 {text-align: center;font-size: 72px;}div.btn-body {display: flex;justify-content: center;align-items: center;border-radius: 30px;background-color: #f0f0f0;padding: 5px;margin: 10px;}p.monitor-head {font-size: 12px;line-height: 2px;}</style><div class=\"main\"><h1>Mini House Hub</h1><div class=\"btn-body\"><button class=\"main-button\" onclick=\"fetch('/api/25-toggle')\">Lights 25</button><button class=\"main-button\" onclick=\"fetch('/api/26-toggle')\">Lights 26</button><button class=\"main-button\" onclick=\"fetch('/api/27-toggle')\">Lights 27</button><button class=\"main-button\" onclick=\"fetch('/api/33-toggle')\">Lights 33</button></div></div></body></html>";
+return "<!DOCTYPE html><html><head><title>The Shack Control</title></head><body><style>html,body {height: 100%;overflow-y: auto;font-family: Arial;}button.main-button {border-radius: 10px;font-size: 30px;background-color: #03CC29;color: white;padding: 10px;margin: 10px;border-color: #02ad22;transition: 0.3s;}div.monitor {border-radius: 10px;font-size: 30px;background-color: #c4c4c4;color: white;padding: 10px;margin: 5px;transition: 0.3s;}button.main-button:hover {background-color: #0CD833;}h1 {text-align: center;font-size: 72px;}div.btn-body {display: flex;justify-content: center;align-items: center;border-radius: 30px;background-color: #f0f0f0;padding: 5px;margin: 10px;}p.monitor-head {font-size: 12px;line-height: 2px;}</style><div class=\"main\"><h1>The High Rolla Controlla</h1><div class=\"btn-body\"><button class=\"main-button\" onclick=\"fetch('/api/toggle')\">Toggle Dah Lights</button></div></div></body></html>";
 }
